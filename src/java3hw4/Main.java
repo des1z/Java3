a02e769ca8504fa9630cd0ec460d137daa377823
viktor <victorchernyaew@gmail.com>
2018-10-04 06:00:05 +0400

    add task1

---
+++ src/java3hw4/Main.java
@@ -0,0 +1,14 @@
+package java3hw4;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+    }
+
+    public static void Task1() {
+        Task1 t = new Task1();
+        new Thread(() -> t.printA()).start();
+        new Thread(() -> t.printB()).start();
+        new Thread(() -> t.printC()).start();
+    }
+}
